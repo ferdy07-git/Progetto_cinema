ee7d8b773b97faf06dc860825a9e4b64fe6cd847
ferdy07-git <ferdydeangeliss@gmail.com>
2026-05-02 20:18:43 +0200

    modifica nomi delle tabelle

--- database/database.php
+++ database/database.php
@@ -1,12 +1,12 @@
 <?php
 require "connessione.php";
 
-$sql = "CREATE TABLE IF NOT EXISTS genere(
+$sql = "CREATE TABLE IF NOT EXISTS Genere(
     id_genere INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(30) NOT NULL
 )";
 $conn->query($sql);
-$sql = "CREATE TABLE IF NOT EXISTS film(
+$sql = "CREATE TABLE IF NOT EXISTS Film(
     id_film INT AUTO_INCREMENT PRIMARY KEY,
     genere INT NOT NULL,
     titolo VARCHAR(50) NOT NULL,
@@ -15,13 +15,13 @@ $sql = "CREATE TABLE IF NOT EXISTS film(
     FOREIGN KEY (genere) REFERENCES genere(id_genere)
 )";
 $conn->query($sql); 
-$sql = "CREATE TABLE IF NOT EXISTS sala(
+$sql = "CREATE TABLE IF NOT EXISTS Sala(
     id_sala INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(30) NOT NULL,
     posti INT NOT NULL 
 )";
 $conn->query($sql);
-$sql = "CREATE TABLE IF NOT EXISTS spettacolo(
+$sql = "CREATE TABLE IF NOT EXISTS Spettacolo(
     id_spettacolo INT AUTO_INCREMENT PRIMARY KEY,
     film INT NOT NULL,
     sala INT NOT NULL,
@@ -31,12 +31,12 @@ $sql = "CREATE TABLE IF NOT EXISTS spettacolo(
     FOREIGN KEY(sala) REFERENCES sala(id_sala)
 )";
 $conn->query($sql);
-$sql = "CREATE TABLE IF NOT EXISTS tipo(
+$sql = "CREATE TABLE IF NOT EXISTS Tipologia_utente(
     id_tipo INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(30) NOT NULL,
 )";
 $conn->query($sql);
-$sql = "CREATE TABLE IF NOT EXISTS utente(
+$sql = "CREATE TABLE IF NOT EXISTS Utente(
     id_utente INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(50) NOT NULL,
     pw VARCHAR(40) NOT NULL,
@@ -45,7 +45,7 @@ $sql = "CREATE TABLE IF NOT EXISTS utente(
     FOREIGN KEY(tipo)  REFERENCES tipo(id_tipo)
 )";
 $conn->query($sql);
-$sql = "CREATE TABLE IF NOT EXISTS biglietto(
+$sql = "CREATE TABLE IF NOT EXISTS Biglietto(
     id_biglietto INT AUTO_INCREMENT PRIMARY KEY,
     importo FLOAT NOT NULL,
     spettacolo int NOT NULL,
