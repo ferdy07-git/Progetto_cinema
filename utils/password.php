82c77f9fdca172981839dbd3ce289719a4bdcd7f
TLKnight997 <tlknight997@gmail.com>
2026-05-04 15:44:31 +0200

    Add files via upload
    utils

---
+++ utils/password.php
@@ -0,0 +1,11 @@
+<?php
+function encrypt($pass): str{
+    return hash('sha256',$pass);
+}
+function check($user,$pass) : boolval {
+    require __DIR__."/../database/connessione.php";
+    $sql = "SELECT password_hash FROM utente WHERE nome = '$user'";
+    $password = $conn->query($sql)->fetch_assoc()["password_hash"];
+    return $password==encrypt($pass);
+}
+?>
