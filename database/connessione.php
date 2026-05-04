f0ade0a243e90f45a7ea89ee159788d370b716ba
TLKnight997 <tlknight997@gmail.com>
2026-05-04 15:52:28 +0200

    Update connessione.php
    Fix connessione

--- database/connessione.php
+++ database/connessione.php
@@ -1,6 +1,9 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "cinema");
-
+$conn = new mysqli("localhost", "root", "", "");
+$sql = "CREATE DATABASE IF NOT EXISTS cinema";
+$conn->query($sql);
+$sql = "USE cinema";
+$conn->query($sql);
 if ($conn->connect_error) {
     die("Errore di connessione: " . $conn->connect_error);
 }
