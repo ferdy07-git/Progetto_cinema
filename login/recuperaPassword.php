e3996bad7e937a043327a77999be78b341673a56
AndreaFratino <andreafratino018@gmail.com>
2026-05-09 17:36:58 +0200

    Refactor password recovery and registration logic

--- login/recuperaPassword.php
+++ login/recuperaPassword.php
@@ -1,31 +1,41 @@
 <?php
-require "../database/connessione.php";
+session_start();
+if(isset($_SESSION["user"])){
+    header("Location:homepage.php");
+}
 require "../utils/password.php";
-
+require "../database/connessione.php";
+$user = $_POST["nick"];
+$pass = $_POST["pass"];
+$conf = $_POST["conferma_pass"];
 $mail = $_POST["email"];
-$p1 = $_POST["pas1"];
-$p2 = $_POST["pas2"];
-
-if ($p1 == $p2) {
-
-    $sql = "SELECT * FROM Utente WHERE email = '$mail'";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {  
-        $password = encrypt($p1);
-        $update = "UPDATE utente SET password_hash = '$password' WHERE email = '$mail'";
-        $conn->query($update);        
-        header("Location: ../homepage.php");
-        exit();
-
-    } else {        
-        header("Location: recupera_password.html?errore=credenziali");
-
-        exit();
-    }
-} else {
+if(!($pass == $conf)){
     
-    header("Location: recupera_password.html?errore=credenziali");
-    exit();
+    $_SESSION["check"] = TRUE;
+    header("Location:reg.php");
+}else if{
+    $sql = "SELECT nome FROM utente WHERE nome = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $nome);
+$stmt->execute();
+
+$result = $stmt->get_result();
+
+if ($result->num_rows > 0) {
+    $_SESSION["check"] = TRUE;
+    header("Location:reg.php");
+}
+}
+else{
+    $password = encrypt($pass);
+    $sql = "INSERT INTO utente VALUES(NULL,'$user','$password','$mail',1)";
+    if($conn->query($sql)==true){
+        $_SESSION["user"] = $user;
+        $_SESSION["password"] = $password;
+        header("Location: ../homepage.php");
+    }else{
+        $_SESSION["check"] = TRUE;
+        header("Location:reg.php");
+    }
 }
 ?>
