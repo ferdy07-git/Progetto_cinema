77186e68bf8a42fb1428b485d6546584cfa8e650
TLKnight997 <tlknight997@gmail.com>
2026-05-15 21:53:27 +0200

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -76,7 +76,7 @@
 
             
             <?php if (!isset($_SESSION['user'])): ?>
-                <a class='btn-accedi' href='./login/form_accesso.php'>
+                <a class='btn-accedi' href='./login/auth/form_accesso.php'>
                     <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><circle cx='12' cy='8' r='5'/><path d='M3 21a9 9 0 0 1 18 0'/></svg>
                     Accedi
                 </a>
