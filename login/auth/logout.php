0a6bc01dbac17b10094e798bb55d73e2026f19ef
TLKnight997 <tlknight997@gmail.com>
2026-05-17 12:26:11 +0200

    Update logout.php

--- login/auth/logout.php
+++ login/auth/logout.php
@@ -1,7 +1,22 @@
 <?php
-    session_start();
-    session_unset();
-    session_destroy();
-    header("Location: ../../homepage.php");
-    exit;
+session_start();
+session_unset();
+session_destroy();
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Logout</title>
+</head>
+<body>
+    <script>
+        // Distrugge la sessione JS
+        sessionStorage.clear();
+
+        // Redirect
+        window.location.href = "../../homepage.php";
+    </script>
+</body>
+</html>
