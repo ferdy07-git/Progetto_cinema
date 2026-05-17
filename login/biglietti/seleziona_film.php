00ac585d85cd9470330d73a8210e7e9c919c297e
TLKnight997 <tlknight997@gmail.com>
2026-05-17 16:39:07 +0200

    Update seleziona_film.php

--- login/biglietti/seleziona_film.php
+++ login/biglietti/seleziona_film.php
@@ -2,11 +2,9 @@
     require "../../database/connessione.php";
     session_start();
     include("../../utils/password.php");
-    if(login()){
-        header("Location:../auth/form_accesso.php");
-    }else{
+    login();
     $_SESSION['id_spettacolo']=$_GET['id_spettacolo'];
     print($_SESSION['id_spettacolo']);
     header("Location:./acquista_biglietto.php");
-    }
+    
 ?>
