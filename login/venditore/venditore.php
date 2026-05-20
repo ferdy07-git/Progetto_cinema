5513e80aa749254aed478ef7e118fe69103ad618
ferdy07-git <ferdydeangeliss@gmail.com>
2026-05-20 10:59:26 +0200

    Update venditore.php

--- login/venditore/venditore.php
+++ login/venditore/venditore.php
@@ -80,7 +80,7 @@ $oggi = date('Y-m-d');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Venditore - Gestione Spettacoli</title>
-    <link rel="stylesheet" href="../style/main.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="../style/style.css?v=<?php echo time(); ?>">
 </head>
 <body class="page-home">
 
