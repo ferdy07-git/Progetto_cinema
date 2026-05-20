cac5d430502445fa36e0d91fd5b5aa7a94b357f6
pa-r48 <pasqualepioradatti2@gmail.com>
2026-05-20 11:15:00 +0200

    reset del branch

--- docker/www/database/connessione.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = new mysqli("db", "root", "", "");
-if ($conn->connect_error) {
-    die("Errore di connessione: " . $conn->connect_error);
-}
-$conn->query("CREATE DATABASE IF NOT EXISTS cinema");
-$conn->select_db("cinema");
-?>
--- docker/www/utils/utils.php
+++
@@ -1,9 +0,0 @@
-<?php
-function passato($data){
-        return $data < date("Y-m-d");
-    }
-function checkmail($mail){
-    $dominio = substr($mail,strpos($mail,"@")+1);
-    return checkdnsrr($dominio, "MX");
-}
-?>
