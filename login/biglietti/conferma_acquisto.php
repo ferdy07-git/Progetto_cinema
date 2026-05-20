bde65246d2830f65b6da1e8bd44be9a55ada6930
ferdy07-git <ferdydeangeliss@gmail.com>
2026-05-20 10:03:02 +0200

    Update conferma_acquisto.php

--- login/biglietti/conferma_acquisto.php
+++ login/biglietti/conferma_acquisto.php
@@ -84,7 +84,8 @@ $totale = $num_posti * $prezzo_unitario;
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700;900&family=DM+Sans:wght@300;400;500;600&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../style/style.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="../style/style.css">
+    <link rel="stylesheet" href="../style/style_conferma.css">
 </head>
 <body>
     <div class="confirm-page">
