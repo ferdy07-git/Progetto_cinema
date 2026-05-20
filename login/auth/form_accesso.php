ef24cf18882cfc97844c0685e76f87316eee5ea4
ferdy07-git <ferdydeangeliss@gmail.com>
2026-05-20 10:00:42 +0200

    Update form_accesso.php

--- login/auth/form_accesso.php
+++ login/auth/form_accesso.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../style/style.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="../style/style.css?v=2">
      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/tabler-icons.min.css">
     <title>Accedi — Cinema Palladino</title>
 </head>
