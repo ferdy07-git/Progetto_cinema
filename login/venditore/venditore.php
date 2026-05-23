95707305c4579c5f0560b362e53eba09670bed26
ferdy07-git <ferdydeangeliss@gmail.com>
2026-05-23 15:41:23 +0200

    Update venditore.php

--- login/venditore/venditore.php
+++ login/venditore/venditore.php
@@ -99,7 +99,6 @@ $oggi = date('Y-m-d');
         </div>
 
         <h2 class="hero-title">Pannello <span>Venditore</span></h2>
-        <p>Vendi biglietti per gli spettacoli in programmazione</p>
 
         <div class="search-bar-wrapper" style="margin-top:14px;">
             <svg class="search-icon" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"/><line x1="21" y1="21" x2="16.65" y2="16.65"/></svg>
